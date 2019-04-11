841e5d3ecfd525f27231d3ed643150367ababf86
Borewit <borewit@users.noreply.github.com>
2019-04-11 07:56:01 +0200

    #Borewit/music-metadata#199 Add loop to keep reading from the same stream

--- listen-to-stream.js
+++ listen-to-stream.js
@@ -1,9 +1,9 @@
-const http = require('http');
-const mm = require('music-metadata');
-const {createLogger, format, transports} = require('winston');
-const {combine} = format;
+const http = require('http')
+const mm = require('music-metadata')
+const {createLogger, format, transports} = require('winston')
+const {combine} = format
 
-const colorizer = format.colorize();
+const colorizer = format.colorize()
 
 const logger = createLogger({
   level: 'info',
@@ -19,58 +19,61 @@ const logger = createLogger({
       colorizer.colorize(msg.level, `${msg.timestamp} - ${msg.level}: ${msg.message}`)
     )
   ),
-});
+})
 
-let stream;
-let mimeType;
-const oggStreamUrl = 'http://stream.radioreklama.bg:80/nrj_low.ogg';
+let stream
+let mimeType
+const oggStreamUrl = 'http://stream.radioreklama.bg:80/nrj_low.ogg'
 
-function httpGet(url) {
+function httpGet (url) {
   return new Promise(function (resolve, reject) {
     http.get(url, function (res) {
       switch (res.statusCode) {
         case 200:
-          resolve(res);
-          break;
+          resolve(res)
+          break
         case 302: // redirect
-          resolve(httpGet(res.headers.location));
-          break;
+          resolve(httpGet(res.headers.location))
+          break
         default:
-          reject(new Error('Unexpected status-code:' + res.statusCode));
+          reject(new Error('Unexpected status-code:' + res.statusCode))
       }
-    });
-  });
+    })
+  })
 }
 
-async function streamForever() {
+async function streamForever () {
   // Get http content
-  stream = await httpGet(oggStreamUrl, {native: true});
-  mimeType = stream.headers['content-type'];
+  stream = await httpGet(oggStreamUrl, {native: true})
+  mimeType = stream.headers['content-type']
 
   if (stream && mimeType) {
-    await mm.parseStream(stream, mimeType, {
-      native: false, skipPostHeaders: true, observer: metaEvent => {
+    while (stream.readable) {
+      await mm.parseStream(stream, mimeType, {
+        native: false, skipPostHeaders: true, observer: metaEvent => {
 
-        logger.info(`type=${metaEvent.tag.type} ${metaEvent.tag.id}=${metaEvent.tag.value}`);
-      }
-    });
-    logger.info('End of never ending Ogg stream?');
+          logger.info(`type=${metaEvent.tag.type} ${metaEvent.tag.id}=${metaEvent.tag.value}`)
+        }
+      })
+      logger.info('End ogg file, proceed to read from the same stream')
+    }
+    logger.info('End of never ending Ogg stream?')
   } else
-    throw new Error('Missing stream or mimeType'); // No need to return explicit promise in async function
+    throw new Error('Missing stream or mimeType') // No need to return explicit promise in async function
 }
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-async function live() {
-  logger.info('Live started.');
+async function live () {
+  logger.info('Live started.')
   do {
-    logger.info('Iteration started.');
-    await streamForever();
-    logger.info('Stream interrupted.');
-    await sleep(4000); // Don't reconnect directly, be gentle
-  } while (1);
+    logger.info('Iteration started.')
+    await streamForever()
+    logger.info('Stream interrupted.')
+    await sleep(4000) // Don't reconnect directly, be gentle
+  } while (1)
 }
 
-live();
+live()
